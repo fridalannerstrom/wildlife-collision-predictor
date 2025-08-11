160b2c456f5220644f5c3a452255ab3a576d4a73
fridalannerstrom <frida.lannerstrom@tqnordic.com>
2025-08-11 15:40:46 +0200

    fix: remove unused pages

--- env_test.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("Loaded URL:", os.getenv("CLEAN_DATA_URL"))
--- test_link.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-url = "https://github.com/fridalannerstrom/wildlife-collision-predictor/releases/download/data/cleaned_data.csv"
-df = pd.read_csv(url, encoding="latin1")
-print(df.head())
